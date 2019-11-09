1169ebae076981c79f782f91dc860f9ee6a3ad28
L.S.S <30771484+lss4@users.noreply.github.com>
2019-11-09 17:05:21 +0900

    Adjusted for building Havoc 2.9.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_maple.mk
+    $(LOCAL_DIR)/havoc_maple.mk
--- havoc_maple.mk
+++ havoc_maple.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/sony/maple-common/maple_common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_maple
+PRODUCT_NAME := havoc_maple
 PRODUCT_DEVICE := maple
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
